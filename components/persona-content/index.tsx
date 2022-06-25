4fd6e746647ee8810abcd2efd06c274e27357f28
arlops22 <ary.s.lopes22@gmail.com>
2022-06-24 23:14:08 -0300

    adicionando keys

--- components/persona-content/index.tsx
+++ components/persona-content/index.tsx
@@ -39,7 +39,7 @@ const PersonaContent = (props: Props) => {
                             // height={300}
                             // width={600}
                         />
-                        <p className={style.slogan}>"{slogan}"</p>
+                        <p className={style.slogan}>{slogan}</p>
                     </div>
                 </div>
                 <div className="col-md-4">
